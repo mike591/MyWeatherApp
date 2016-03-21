b9abef7c4e41587a49a42100f45451e2b5b1f9c8
mike591 <mikemach123@sbcglobal.net>
2016-03-21 15:55:19 -0700

    Displays Oakland, US weather and temperature.

--- app/src/main/java/mmach/android/myweatherapp/GetMyWeather.java
+++ app/src/main/java/mmach/android/myweatherapp/GetMyWeather.java
@@ -3,6 +3,10 @@ package mmach.android.myweatherapp;
 import android.net.Uri;
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -22,19 +26,53 @@ public class GetMyWeather {
     private static final String COUNTRY = "USA";
     private static final String FAHRENHEIT = "imperial";
 
-    private static final Uri ENDPOINT = Uri.parse("api.openweathermap.org/data/2.5/weather")
+    public static final Uri ENDPOINT = Uri.parse("http://api.openweathermap.org/data/2.5/weather")
             .buildUpon()
-            .appendQueryParameter("q ", CITY + ',' + COUNTRY)
+            .appendQueryParameter("q", CITY + ',' + COUNTRY)
+            .appendQueryParameter("APPID", API_KEY)
             .appendQueryParameter("units", FAHRENHEIT)
-            .appendQueryParameter("AAPID", API_KEY)
             .build();
 
-    public byte[] connectAndGetData() throws IOException {
-        URL url = new URL(ENDPOINT.toString());
-        HttpURLConnection hcon = (HttpURLConnection) url.openConnection();
+    public static WeatherItem generateWeatherData() {
+        WeatherItem weatherItem = new WeatherItem();
+        Log.i(TAG, "Now using generateWeatherData");
 
         try {
+            JSONObject jBody = new JSONObject(convertDataToString());
 
+            JSONArray jWeathers = jBody.getJSONArray("weather");
+            JSONObject jWeather = jWeathers.getJSONObject(1);
+
+            JSONObject jDetail = jBody.getJSONObject("main");
+
+            JSONObject jSys = jBody.getJSONObject("sys");
+
+            Log.i(TAG, "The Weather: " + jWeather.getString("main"));
+            weatherItem.setWeather(jWeather.getString("main"));
+
+            Log.i(TAG, "The Location: " + jBody.getString("name") + ", " + jSys.get("country"));
+            weatherItem.setLocation(jBody.getString("name") + ", " + jSys.get("country"));
+
+            Log.i(TAG, "The High Temp: " + jDetail.getString("temp_max"));
+            weatherItem.setHighTemperature(jDetail.getString("temp_max"));
+
+            Log.i(TAG, "The Low Temp: " + jDetail.getString("temp_min"));
+            weatherItem.setLowTemperature(jDetail.getString("temp_min"));
+        } catch (IOException ioe) {
+            Log.d(TAG, "generateWeatherData: " + ioe);
+        } catch (JSONException j) {
+            Log.d(TAG, "generateWeatherData: " + j);
+        }
+
+        return weatherItem;
+    }
+
+    public static byte[] connectAndGetData() throws IOException {
+        URL url = new URL(ENDPOINT.toString());
+        HttpURLConnection hcon = (HttpURLConnection) url.openConnection();
+        Log.i(TAG, "Connection to " + ENDPOINT);
+
+        try {
             InputStream in = hcon.getInputStream();
             byte[] buffer = new byte[1024];
             ByteArrayOutputStream out = new ByteArrayOutputStream();
@@ -43,16 +81,14 @@ public class GetMyWeather {
             while ((bytesRead = in.read(buffer)) > 0) {
                 out.write(buffer, 0, bytesRead);
             }
-
             return out.toByteArray();
         } finally {
             hcon.disconnect();
         }
     }
 
-    public WeatherItem generateWeatherData() {
-        return null;
+    public static String convertDataToString() throws IOException {
+        return new String(connectAndGetData());
     }
-
 
 }
--- app/src/main/java/mmach/android/myweatherapp/WeatherFragment.java
+++ app/src/main/java/mmach/android/myweatherapp/WeatherFragment.java
@@ -1,28 +1,74 @@
 package mmach.android.myweatherapp;
 //http://openweathermap.org/api
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.json.JSONException;
+
+import java.io.IOException;
+
 /**
  * Created by Mike on 3/20/2016.
  */
 public class WeatherFragment extends Fragment {
+    private static final String TAG = "WeatherFragment";
 
     private ImageView mImageView;
     private TextView mWeatherTextView;
     private TextView mLocationTextView;
+    private TextView mHighsTextView;
+    private TextView mLowsTextView;
 
+    private WeatherItem mWeatherItem;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new GenerateWeatherData().execute();
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.weather_fragment, container, false);
+
+        View v = inflater.inflate(R.layout.weather_fragment, container, false);
+        mWeatherTextView = (TextView) v.findViewById(R.id.weatherTextView);
+        mLocationTextView = (TextView) v.findViewById(R.id.locationTextView);
+        mHighsTextView = (TextView) v.findViewById(R.id.highTemp);
+        mLowsTextView = (TextView) v.findViewById(R.id.lowTemp);
+
+        return v;
+    }
+
+    public void updateWeather() {
+        mWeatherTextView.setText(mWeatherItem.getWeather());
+        mLocationTextView.setText(mWeatherItem.getLocation());
+        mHighsTextView.setText(mWeatherItem.getHighTemperature());
+        mLowsTextView.setText(mWeatherItem.getLowTemperature());
+    }
+
+
+    private class GenerateWeatherData extends AsyncTask<Void, Void, WeatherItem> {
+
+        @Override
+        protected WeatherItem doInBackground(Void... params) {
+            return GetMyWeather.generateWeatherData();
+        }
+
+        @Override
+        protected void onPostExecute(WeatherItem weatherItem) {
+            mWeatherItem = weatherItem;
+            updateWeather();
+        }
     }
 }
